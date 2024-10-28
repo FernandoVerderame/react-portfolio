e39dcf338ffead9b73ac950463c4cc1f065fd18b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-10-28 18:23:53 +0100

    fixed jumbotron

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -5,7 +5,7 @@ import imgPortfolio from "../../assets/images/img-portfolio.jpeg";
 const Jumbotron = () => {
     return (
         <>
-            <div id={`${jumbotronStyle.jumbo}`}>
+            <section id={`${jumbotronStyle.jumbo}`}>
                 <div className="container">
                     <div className="row">
                         <div className="col-6">
@@ -29,7 +29,7 @@ const Jumbotron = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </>
     );
 };
