3e9b627f095210cf4f7276f26a9714b6887d4791
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-10-28 16:24:24 +0100

    added jumbotron

---
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+import jumbotronStyle from "./Jumbotron.module.scss";
+import imgPortfolio from "../../assets/images/img-portfolio.jpeg";
+
+const Jumbotron = () => {
+    return (
+        <>
+            <div id={`${jumbotronStyle.jumbo}`}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <span className={`${jumbotronStyle.hello}`}>Ciao! Sono</span>
+                            <h1 className={`${jumbotronStyle.title}`}><span>Fernando Verderame</span><br />Jr. Full-Stack Web Developer</h1>
+                            <p className={`${jumbotronStyle.description}`}>Costruisco soluzioni web che uniscono creatività e funzionalità, perché credo che ogni linea di codice possa migliorare il modo in cui le persone vivono il digitale.</p>
+                            <div className={`${jumbotronStyle.buttonGroup}`}>
+                                <div>
+                                    <NavLink className={`${jumbotronStyle.button}`}>
+                                        Su Di me
+                                    </NavLink>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-6 d-flex justify-content-center align-items-center">
+                            <img
+                                src={imgPortfolio}
+                                alt="img-portfolio"
+                                className={`${jumbotronStyle.img}`}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Jumbotron;
