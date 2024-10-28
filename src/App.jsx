b08135342fecca144862015ed27681efbbf2f6bc
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-10-28 16:27:17 +0100

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Jumbotron from "./components/Jumbotron/Jumbotron.jsx";
+
 function App() {
 
   return (
     <>
-
+      <Navbar />
+      <Jumbotron />
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
