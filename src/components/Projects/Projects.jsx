2a7ef44e62fb49998131be1731f2bc089622355f
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-10-29 16:10:12 +0100

    added projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,42 +1,50 @@
 import projectsStyle from "./Projects.module.scss";
 import { FaGithub as GitHub } from "react-icons/fa";
+import projects from "../../database/projects.js"
 
 const Projects = () => {
+
     return (
         <>
             <section id={`${projectsStyle.projects}`}>
                 <div className="container">
-                    <div className="row g-3 align-items-center justify-content-center">
+                    <div className="row g-5 align-items-center justify-content-center">
                         <div className="col-12">
                             <h2 className={`${projectsStyle.title}`}>Progetti</h2>
                         </div>
 
-                        <div className="col-12">
-                            <div className={`${projectsStyle.card}`}>
-                                <div className={`${projectsStyle.leftCard}`}>
-                                    <h3 className={`${projectsStyle.projectTitle}`}>Titolo</h3>
-                                    <div className={`${projectsStyle.descriptionContainer}`}>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate mollitia a non dolore rerum aliquam eligendi, similique, incidunt repudiandae pariatur perferendis enim? Dolorum maiores numquam hic adipisci iste nam non.
-                                            Fugit voluptates iusto delectus nesciunt obcaecati autem sit libero, rerum atque dolorum quae optio doloribus exercitationem labore? Doloribus dolorem quibusdam at ab quisquam modi, id ullam illum eveniet esse velit!
-                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore iusto doloremque, corrupti magnam asperiores omnis quis nemo consectetur maiores facilis iste nisi voluptate ipsum tenetur non nihil quia repellat suscipit.
-                                            Reiciendis quasi nemo et atque inventore quisquam veritatis recusandae deleniti tenetur? Ducimus voluptas autem aliquam et ad, explicabo alias laboriosam rem animi, voluptate dolore minus soluta nihil nulla id omnis?</p>
-                                    </div>
-                                    <h4 className={`${projectsStyle.technologiesTitle}`}>Tecnologie utilizzate:</h4>
-                                    <ul className={`${projectsStyle.technologiesList}`}>
-                                        <li>html</li>
-                                        <li>scss</li>
-                                        <li>javascript</li>
-                                    </ul>
-                                    <div className={`${projectsStyle.divider}`}></div>
-                                    <div className={`${projectsStyle.btnSection}`}>
-                                        <GitHub className={`${projectsStyle.button}`} />
-                                    </div>
-                                </div>
-                                <div className={`${projectsStyle.rightCard}`}>
+                        {projects.map((project, index) => (
+                            <div key={index} className="col-12">
+                                <div className={projectsStyle.card}>
+                                    <div className={projectsStyle.leftCard}>
 
+                                        <div className="top">
+                                            <h3 className={projectsStyle.projectTitle}>{project.title}</h3>
+                                            <div className={projectsStyle.descriptionContainer}>
+                                                <p>{project.description}</p>
+                                            </div>
+                                        </div>
+
+                                        <div className="bottom">
+                                            <h4 className={projectsStyle.technologiesTitle}>Tecnologie utilizzate:</h4>
+                                            <ul className={projectsStyle.technologiesList}>
+                                                {project.technologies.map((tech, i) => (
+                                                    <li key={i}>{tech}</li>
+                                                ))}
+                                            </ul>
+                                            <div className={projectsStyle.divider}></div>
+                                            <div className={projectsStyle.btnSection}>
+                                                <GitHub className={projectsStyle.button} />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className={projectsStyle.rightCard}>
+
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </section>
---
+++ src/database/projects.js
@@ -0,0 +1,54 @@
+const projects = [
+    {
+        "title": "Portfolio Personale",
+        "description": "Un sito web personale per mostrare il portfolio di progetti. Include una sezione con dettagli sullo sviluppatore, una gallery di progetti, e una pagina di contatto.Un sito web personale per mostrare il portfolio di progetti. Include una sezione con dettagli sullo sviluppatore, una gallery di progetti, e una pagina di contatto.Un sito web personale per mostrare il portfolio di progetti. Include una sezione con dettagli sullo sviluppatore, una gallery di progetti, e una pagina di contatto.Un sito web personale per mostrare il portfolio di progetti. Include una sezione con dettagli sullo sviluppatore, una gallery di progetti, e una pagina di contatto.",
+        "technologies": ["HTML", "SCSS", "JavaScript", "React", "Node.js"],
+        "images": [
+            "https://example.com/portfolio1.png",
+            "https://example.com/portfolio2.png",
+            "https://example.com/portfolio3.png"
+        ]
+    },
+    {
+        "title": "E-commerce Responsive",
+        "description": "Una piattaforma e-commerce responsive sviluppata per vendita di prodotti tecnologici, completa di gestione prodotti, carrello e funzionalità di checkout sicuro.",
+        "technologies": ["HTML", "CSS", "JavaScript", "React", "Firebase"],
+        "images": [
+            "https://example.com/ecommerce1.png",
+            "https://example.com/ecommerce2.png",
+            "https://example.com/ecommerce3.png"
+        ]
+    },
+    {
+        "title": "Dashboard Analitica",
+        "description": "Una dashboard web per la visualizzazione dei dati aziendali con grafici interattivi e filtri avanzati per un'analisi completa e in tempo reale.",
+        "technologies": ["HTML", "SCSS", "JavaScript", "Vue.js", "Chart.js"],
+        "images": [
+            "https://example.com/dashboard1.png",
+            "https://example.com/dashboard2.png",
+            "https://example.com/dashboard3.png"
+        ]
+    },
+    {
+        "title": "App per il Meteo",
+        "description": "Un'applicazione che fornisce le previsioni meteo in tempo reale per le città selezionate. Include una grafica moderna con icone e animazioni per rappresentare i vari fenomeni metereologici.",
+        "technologies": ["HTML", "CSS", "JavaScript", "React", "OpenWeather API"],
+        "images": [
+            "https://example.com/weather1.png",
+            "https://example.com/weather2.png",
+            "https://example.com/weather3.png"
+        ]
+    },
+    {
+        "title": "Social Media Feed",
+        "description": "Un'applicazione web che simula un feed di social media, con funzioni di pubblicazione, like, e commenti per post pubblicati dagli utenti.",
+        "technologies": ["HTML", "CSS", "JavaScript", "Angular", "Firebase"],
+        "images": [
+            "https://example.com/social1.png",
+            "https://example.com/social2.png",
+            "https://example.com/social3.png"
+        ]
+    }
+];
+
+export default projects;
