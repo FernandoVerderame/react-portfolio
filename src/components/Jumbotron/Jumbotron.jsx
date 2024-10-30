102ba2783b7e016694863ea31f2d4ab7150d169f
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-10-30 15:44:19 +0100

    added responsive jumbotron

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -7,8 +7,8 @@ const Jumbotron = () => {
         <>
             <section id={`${jumbotronStyle.jumbo}`}>
                 <div className="container">
-                    <div className="row">
-                        <div className="col-6">
+                    <div className="row g-5">
+                        <div className="col-lg-6">
                             <span className={`${jumbotronStyle.hello}`}>Ciao! Sono</span>
                             <h1 className={`${jumbotronStyle.title}`}><span>Fernando Verderame</span><br />Jr. Full-Stack Web Developer</h1>
                             <p className={`${jumbotronStyle.description}`}>Costruisco soluzioni web che uniscono creatività e funzionalità, perché credo che ogni linea di codice possa migliorare il modo in cui le persone vivono il digitale.</p>
@@ -20,7 +20,7 @@ const Jumbotron = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-6 d-flex justify-content-center align-items-center">
+                        <div className="col-lg-6 d-flex justify-content-center align-items-center">
                             <img
                                 src={imgPortfolio}
                                 alt="img-portfolio"
