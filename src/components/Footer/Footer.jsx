8d6160a613231c8d404b3bfab17b9fe14b765ff5
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-11-05 11:37:56 +0100

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 const Footer = () => {
     return (
         <>
-
+            <footer style={{ backgroundColor: '#2e2e2e', padding: '10px' }}>
+                <div className="container">
+                    <p className="m-0 text-white">Made with &#9829; by <span className="fst-italic">Fernando Verderame</span></p>
+                </div>
+            </footer >
         </>
     );
 };
